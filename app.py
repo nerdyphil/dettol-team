970169426f7f90bcf7412a2d1469a96d4c4c69c8
Ifihanagbara Olusheye <victoriaolusheye@gmail.com>
2020-07-11 19:50:44 +0100

    Adding fixes

--- app.py
+++ app.py
@@ -1,9 +1,15 @@
+from threading import Timer
 import os
+from urllib.parse import urljoin
 from PIL import Image, ImageFont, ImageDraw
-from flask import Flask, request
+from flask import Flask, request, url_for, redirect
 
 app = Flask(__name__)
-
+BASE_PATH = os.path.dirname(__file__)
+STATIC_PATH = os.path.join(BASE_PATH, "static")
+FONT_PATH = os.path.join(STATIC_PATH, "fonts")
+CERTIFICATE_PATH = os.path.join(STATIC_PATH, "certificates")
+GENERATED_PATH = os.path.join(STATIC_PATH, "generated")
 
 @app.route("/")
 def index():
@@ -12,42 +18,88 @@ def index():
 
 @app.route("/generate/")
 def generate():
-    certificate = make_certificate(**request.args)
-    return certificate
+    certificate = make_certificate("jersey.jpeg", **request.args)
+    return redirect(certificate)
 
+def delete_file(img_title):
+    os.unlink(os.path.join(GENERATED_PATH, img_title))
 
-def make_certificate(username, jerseynumber, type, track=None):
-    def draw_text(filename, type, username, jerseynumber, track=None):
-        font = "PTSans-Bold.ttf"
-        color = "#ff0000"
-        size = 50
-        track_color = "#000000"
-        track_size = 20
-        y = 350
-        x = 0
-        text = "{} {}".format(username, jerseynumber).upper()
-        if type == "2":
-            font = "LeagueSpartan-Bold.otf"
-            size = 60
-            color = "#e05a47"
-            y = 175
-            x = 88
-            text = "{}\n{}".format(username, jerseynumber).upper()
-        raw_img = Image.open(os.path.join("certificates", filename))
-        img = raw_img.copy()
-        draw = ImageDraw.Draw(img)
+def make_certificate(username, number):
+    # set certificate style
+    font = "PTSans-Bold.ttf"
+    track_font = "LeagueSpartan-Bold.otf"
 
-        # draw name
-        if username:
-            PIL_font = ImageFont.truetype(os.path.join("fonts", font), size)
-            w, h = draw.textsize(text, font=PIL_font)
-            W, H = img.size
-            x = (W - w) / 2 if x == 0 else x
-            draw.text((x, y), text, fill=color, font=PIL_font)
-        img_url = os.path.join("static", "{}-{}-Join-Me-On-The-Cool-Team.png".format(username, jerseynumber, type))
-        img.save(img_url)
-        return request.host_url + img_url
+    # name style
+    color = "#c9a04b"
+    size = 70
+    y = 645
 
+    # track style
+    track_color = "#ffffff"
+    track_size = 40
+
+    # name text
+    text = "{} {}".format(username, number).upper()
+    raw_img = Image.open(os.path.join(CERTIFICATE_PATH, filename))
+    img = raw_img.copy()
+    draw = ImageDraw.Draw(img)
+
+    # draw name
+    PIL_font = ImageFont.truetype(os.path.join(FONT_PATH, font), size)
+    w, h = draw.textsize(text, font=PIL_font)
+    W, H = img.size
+    x = (W - w) / 2
+    draw.text((x, y), text, fill=color, font=PIL_font)
+
+    # draw number
+    PIL_font = ImageFont.truetype(os.path.join(FONT_PATH, track_font), track_size)
+    track_text = "{} {}".format(username, jersey)
+    w, h = draw.textsize(track_text, font=PIL_font)
+    W, H = img.size
+    x, y = (W - w) / 2, 840
+    draw.text((x, y), track_text, fill=track_color, font=PIL_font)
+
+    # save certificate
+    img_title = "{}-{}.png".format(username, number)
+    img.save(os.path.join(GENERATED_PATH, img_title))
+    task = Timer(30, delete_file, (img_title,))
+    task.start()
+    base_64 =  urljoin(request.host_url, url_for("static", filename="generated/" + img_title))
+
+    return base_64
+# def make_certificate(username, jerseynumber, type, track=None):
+#     def draw_text(filename, type, username, jerseynumber, track=None):
+#         font = "PTSans-Bold.ttf"
+#         color = "#ff0000"
+#         size = 50
+#         track_color = "#000000"
+#         track_size = 20
+#         y = 350
+#         x = 0
+#         text = "{} {}".format(username, jerseynumber).upper()
+#         if type == "2":
+#             font = "LeagueSpartan-Bold.otf"
+#             size = 60
+#             color = "#e05a47"
+#             y = 175
+#             x = 88
+#             text = "{}\n{}".format(username, jerseynumber).upper()
+#         raw_img = Image.open(os.path.join("certificates", filename))
+#         img = raw_img.copy()
+#         draw = ImageDraw.Draw(img)
+
+#         # draw name
+#         if username:
+#             PIL_font = ImageFont.truetype(os.path.join("fonts", font), size)
+#             w, h = draw.textsize(text, font=PIL_font)
+#             W, H = img.size
+#             x = (W - w) / 2 if x == 0 else x
+#             draw.text((x, y), text, fill=color, font=PIL_font)
+#         img_url = os.path.join("static", "{}-{}-Join-Me-On-The-Cool-Team.png".format(username, jerseynumber, type))
+#         img.save(img_url)
+#         return request.host_url + img_url
+
+#2
         # draw track
         #if track:
         #    PIL_font = ImageFont.truetype(os.path.join("fonts", font), track_size)
@@ -58,8 +110,10 @@ def make_certificate(username, jerseynumber, type, track=None):
         #img.save(img_url)
         #return request.host_url + img_url
 
-    base_64 = draw_text("framed.png", type, username, jerseynumber)
-    return base_64
+#new code
+    # base_64 = draw_text("framed.png", type, username, jerseynumber)
+    # return base_64
+
     #tracks = {"frontend": "Front-End Web Development", "backend": "Back-End Web Development", "python": "Python Programming", "android": "Mobile Development", "ui": "UI/UX Design", "design": "Engineering Design"}
     #track = tracks.get(track, None)
     #    base_64 = draw_text("framed.png", type, first_name, last_name, track)
